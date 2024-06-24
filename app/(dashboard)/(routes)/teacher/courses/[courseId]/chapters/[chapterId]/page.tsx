a7fa83c4646ac76b5f00e4da5f737082097f8562
Phan Ngoc Hoang Anh <teoanhss113@gmail.com>
2024-06-24 11:40:09 +0700

    courses data table

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,9 +1,13 @@
 import { IconBadge } from '@/components/icon-badge'
+import Banner from '@/components/banner'
 import { db } from '@/lib/db'
 import { auth } from '@clerk/nextjs/server'
-import { ArrowLeft, LayoutDashboard } from 'lucide-react'
+import { ArrowLeft, Eye, LayoutDashboard } from 'lucide-react'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
+import ChapterTitleForm from './_components/chapter-title-form'
+import ChapterDescriptionForm from './_components/chapter-description-form'
+import ChapterAccessForm from './_components/chapter-access-form'
 
 export default async function ChapterIdPage({
   params,
@@ -36,38 +40,67 @@ export default async function ChapterIdPage({
 
   const completionText = `(${completedFields}/${totalFields})`
 
+  const isComplete = requiredFields.every(Boolean)
   return (
-    <div className="p-6">
-      <div className="flex items-center justify-between">
-        <div className="w-full">
-          <Link
-            href={`/teacher/courses/${params.courseId}`}
-            className="flex items-center text-sm hover:opacity-75 transition mb-6"
-          >
-            <ArrowLeft className="h-4 w-4 mr-2" />
-            Quay lại khoá học
-          </Link>
-          <div className="flex items-center justify-between w-full">
-            <div className="flex flex-col gap-y-2">
-              <h1 className="text-2xl font-medium">Thiết lập bài học</h1>
-              <span className="text-sm text-slate-700">
-                Hoàn thành tất cả các trường {completionText}
-              </span>
+    <>
+      {!chapter.isPublished && (
+        <Banner
+          variant="warning"
+          label="Bài học này chưa được công khai. Nó sẽ không hiển thị trong khoá học"
+        />
+      )}
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="w-full">
+            <Link
+              href={`/teacher/courses/${params.courseId}`}
+              className="flex items-center text-sm hover:opacity-75 transition mb-6"
+            >
+              <ArrowLeft className="h-4 w-4 mr-2" />
+              Quay lại khoá học
+            </Link>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">Thiết lập bài học</h1>
+                <span className="text-sm text-slate-700">
+                  Hoàn thành tất cả các trường {completionText}
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div className="space-y-4">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={LayoutDashboard} />
+                <h2 className="text-xl">Tinh chỉnh bài học</h2>
+              </div>
+              <ChapterTitleForm
+                initialData={{ title: chapter.title }}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
+              <ChapterDescriptionForm
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={Eye} />
+                <h2>Cài đặt truy cập</h2>
+              </div>
+              <ChapterAccessForm
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+              />
             </div>
           </div>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-        <div className="space-y-4">
-          <div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={LayoutDashboard} />
-              <h2 className="text-xl">Tinh chỉnh bài học</h2>
-            </div>
-            {/* Chapter */}
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
